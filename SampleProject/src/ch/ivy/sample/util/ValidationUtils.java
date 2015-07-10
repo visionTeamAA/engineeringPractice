5e1a549a7e7cbb3e0638e956fdf786dae4eddfa5
xnvinh <xnvinh@users.noreply.github.com>
2015-07-10 17:05:43 +0700

    add validation in step 1

--- SampleProject/src/ch/ivy/sample/util/ValidationUtils.java
+++ SampleProject/src/ch/ivy/sample/util/ValidationUtils.java
@@ -148,7 +148,7 @@ public class ValidationUtils {
 			
 			if (isClearErrorAfterSending){
 				this.errorString = ConstantVariable.EMPTY_STRING;
-				this.validationItems = null;
+				this.validationItems.clear();
 			}
 		}
 	}
