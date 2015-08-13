ec09c881760a808ca0afa8c4122c575fecd6cf0f
vhsvuong <son_vuong_2008@yahoo.com>
2015-08-13 11:06:35 +0700

    Load data with dynamic columns

---
+++ src_dataClasses/company/team/vision/databaseJava/page1/page1Data.java
@@ -0,0 +1,12 @@
+package company.team.vision.databaseJava.page1;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.Generated(comments="This is the java file of the ivy data class page1Data", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class page1Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 0L;
+
+}
---
+++ src_dataClasses/company/team/vision/databaseJava/page2/page2Data.java
@@ -0,0 +1,12 @@
+package company.team.vision.databaseJava.page2;
+
+/**
+ */
+@SuppressWarnings("all")
+@javax.annotation.Generated(comments="This is the java file of the ivy data class page2Data", value={"ch.ivyteam.ivy.scripting.streamInOut.IvyScriptJavaClassBuilder"})
+public class page2Data extends ch.ivyteam.ivy.scripting.objects.CompositeObject
+{
+  /** SerialVersionUID */
+  private static final long serialVersionUID = 0L;
+
+}
